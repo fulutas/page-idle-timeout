fa8fb7963707d0da94b524d45c3c5f5fc351d778
Furkan Ulutaş <37690550+fulutas@users.noreply.github.com>
2022-08-05 16:25:07 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import useUserActive from "./useUserActive";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const { isExpire } = useUserActive();
+
+watch(isExpire, (newValue, oldValue) => {
+  console.log('isExpireNewValue : ', newValue);
+  console.log('isExpireOldValue : ', oldValue);
+}, { deep: true  });
+
+</script>
